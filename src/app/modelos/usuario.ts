62b4ea99c8c90423f52ee950779445fe1e95ec3f
Joao Vitor <clauzer75@hotmail.com>
2020-06-19 10:41:51 -0300

    usuario

---
+++ src/app/modelos/usuario.ts
@@ -0,0 +1,13 @@
+export class usuario {
+    public Id: number;
+    public nome: string;
+    public senha:string;
+    public email:string;
+    
+    constructor(Id:number,nome: string,senha:string,email:string) {
+    this.Id = Id;
+    this.nome = nome;
+    this.senha = senha;
+    this.email = email;
+    }
+}
