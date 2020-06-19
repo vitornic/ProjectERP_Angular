cad241ad746b59c281ed169547d72ed2c1665650
lucas <lucasfilms020@gmail.com>
2020-06-19 00:48:52 -0300

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ModulosComponent } from './pages/modulos/modulos.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ModulosComponent } from './pages/modulos/modulos.component';
     ModulosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
