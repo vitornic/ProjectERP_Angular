4bc6286a36b884b6f6950bdef17bad215d744be1
Joao Vitor <clauzer75@hotmail.com>
2020-06-19 00:51:24 -0300

    fix modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
-import { ModulosComponent } from './pages/modulos/modulos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    ModulosComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule
