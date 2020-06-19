3a936387a34a57aade5686b2ec517cc417114175
Joao Vitor <clauzer75@hotmail.com>
2020-06-19 01:04:42 -0300

    dd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '/', component: LoginComponent }
+  { path: '**', component: LoginComponent }
   ];
 
 @NgModule({
