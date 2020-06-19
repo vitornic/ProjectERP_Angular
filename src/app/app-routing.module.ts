4997cb56a997aa43fa4aa0f4c0657500a3da6b6e
Joao Vitor <clauzer75@hotmail.com>
2020-06-19 03:52:36 -0300

    remove empty route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
   { path: 'Login', component: LoginComponent },
   { path: '**', redirectTo: 'Login' }
 ];
